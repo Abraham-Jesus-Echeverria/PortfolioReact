1258ad25070ccf30cca32da40d3a34ed0a35ad92
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2023-01-27 20:42:44 -0600

    agregando clases al icono whatssap

--- src/components/personalInformation.js
+++ src/components/personalInformation.js
@@ -4,7 +4,7 @@ export default function PersonalInformation({stylesContainer, stylesItems}){
     return(<> 
         <div className={stylesContainer}> 
             <p><i className={`fa-solid fa-envelope ${stylesItems}`}></i>abraham.ech9@gmail.com</p> 
-            <p><i className="fa-brands fa-whatsapp"></i>9994-23-34-16</p> 
+            <p><i className={`fa-brands fa-whatsapp ${stylesItems}`}></i>9994-23-34-16</p> 
             <p><i className={`fa-solid fa-location-dot ${stylesItems}`}></i>Mérida Yucatán México</p>   
         </div>
     </>); 
