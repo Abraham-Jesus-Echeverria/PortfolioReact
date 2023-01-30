9a60456980ccdbe645fbecdc54338be706d36ca4
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2023-01-30 13:51:21 -0600

    buttonXD

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,4 @@
 import React from "react"; 
 
 const Button = ({styles, children, handleButton, referencia}) => <button className={styles} onClick={handleButton} ref={referencia}>{children}</button> 
-
 export default Button; 
